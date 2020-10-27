bda123ce9ddb9b9fdee375ee1834d2436dfa36cd
Denis B <hawk911@yandex.ru>
2020-10-27 08:16:19 +0700

    errors are fixed

--- go-core/lesson_2/cmd/engine/main.go
+++ go-core/lesson_2/cmd/engine/main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 	"pkg/crawler"
 	"strings"
 )
 
+var words string
+
 func main() {
 
-	url := reader("Enter link like https://habr.com: \n")
-	fmt.Println("Scanning...")
+	url := "https://habr.com"
+	fmt.Println("Scanning... ", url)
 	const depth = 2
 	titles, err := crawler.Scan(url, depth)
 	if err != nil {
@@ -22,14 +23,16 @@ func main() {
 	fmt.Println("Welcome to the lesson 2")
 	for {
 		fmt.Println("Enter some words that its need to find or leave empty for exit:")
-		reader := bufio.NewReader(os.Stdin)
-		words, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println(err)
+		words := ""
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			words = scanner.Text()
+			break
+		}
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "Something wrong, try again in a few minutes, please, :", err)
 			return
 		}
-
-		words = strings.TrimSuffix(words, "\r\n")
 
 		if len(words) == 0 {
 			fmt.Println("Thank you to use our solution!")
@@ -37,7 +40,7 @@ func main() {
 		}
 
 		for k, v := range titles {
-			if strings.Contains(k, words) || strings.Contains(v, words) {
+			if strings.Contains(lower(k), lower(words)) || strings.Contains(lower(v), lower(words)) {
 				fmt.Printf("%s - '%s'\n", k, v)
 			}
 		}
@@ -45,22 +48,7 @@ func main() {
 
 }
 
-func reader(text string) string {
-	textStdin := ""
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Print(text)
-	//Использую Scan  - попробовал поработать
-	for scanner.Scan() {
-		textStdin = scanner.Text()
-		textStdin = strings.TrimSpace(textStdin)
-		break
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "Something wrong, try again in a few minutes, please, :", err)
-	}
-	if len(textStdin) < 3 {
-		log.Fatalln("Param is empty or there are less than 3 symbols!")
-	}
-
-	return textStdin
+func lower(str string) string {
+	strLower := strings.ToLower(str)
+	return strLower
 }
